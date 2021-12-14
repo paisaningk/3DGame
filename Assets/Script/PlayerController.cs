b935986a95dce59d71284e950ba28bc80c435aff
paisaningk <gaw.fd@hotmail.com>
2021-12-14 07:06:55 +0700

    walk

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,62 +1,107 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Script
 {
-    [SerializeField] private float speed;
-    [Range(0.1f,10)]
-    [SerializeField] private float gravoty;
-    [SerializeField] private float jumpDistance;
-    
-    private PlayerInput playerInput;
-    private Rigidbody rigidbody;
-    private CharacterController controller;
-    private Vector3 moveDirection = Vector3.zero;
-    
-
-    private void Start()
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerController : MonoBehaviour
     {
-        playerInput = new PlayerInput();
-        rigidbody = GetComponent<Rigidbody>();
-        playerInput.Enable();
-        playerInput.Player.Jump.performed += context => Jump();
-        controller = GetComponent<CharacterController>();
-    }
-
-    private void Update()
-    {
-        Debug.Log(controller.isGrounded);
-    }
-
-    void FixedUpdate()
-    {
-        if (controller.isGrounded)
-        {
-            var move = playerInput.Player.Move.ReadValue<Vector2>();
-            Debug.Log($"Move {move}");
-            moveDirection = new Vector3(move.x,0,move.y);
-            moveDirection *= speed;
-        }
+        [SerializeField] private float speed;
+        [Range(0.1f,10)]
+        [SerializeField] private float gravoty;
+        [SerializeField] private float jumpDistance;
         
-        moveDirection.y -= gravoty;
-        controller.Move(moveDirection);
-    }
-    
-    private void Jump()
-    {
-        if (controller.isGrounded)
-        {
-            moveDirection.y += jumpDistance;
-        }
-        controller.Move(moveDirection);
-    }
+        private float rotationPower = 3f;
+        private float rotationLerp = 0.5f;
+        private Vector2 smoothDeltaPosition = Vector2.zero;
+        private Vector2 move = Vector2.zero;
+        private Vector2 look = Vector2.zero;
+        private Vector3 nextPosition;
+        private Vector2 velocity;
+        private CharacterController controller;
+        public GameObject followTransform;
+        private Quaternion nextRotation;
 
-    // void FixedUpdate()
-    // {
-    //     var walk = playerInput.Player.Move.ReadValue<Vector2>();
-    //     rigidbody.velocity = walk.normalized * speed;
-    // }
+        private void Start()
+        {
+            controller = GetComponent<CharacterController>();
+        }
+
+        public void OnMove(InputValue value)
+        {
+            move = value.Get<Vector2>();
+        }
+
+        public void OnMouse(InputValue value)
+        {
+            look = value.Get<Vector2>();
+        }
+
+        private void Update()
+        {
+            #region Follow Transform Rotation
+
+            //Rotate the Follow Target transform based on the input
+            followTransform.transform.rotation *= Quaternion.AngleAxis(look.x * rotationPower, Vector3.up);
+            followTransform.transform.rotation *= Quaternion.AngleAxis(look.y * rotationPower, Vector3.right);
+
+            var angles = followTransform.transform.localEulerAngles;
+            angles.z = 0;
+
+            var angle = followTransform.transform.localEulerAngles.x;
+
+            //Clamp the Up/Down rotation
+            if (angle > 180 && angle < 340)
+            {
+                angles.x = 340;
+            }
+            else if(angle < 180 && angle > 40)
+            {
+                angles.x = 40;
+            }
+
+
+            followTransform.transform.localEulerAngles = angles;
+            #endregion
+            
+            nextRotation = Quaternion.Lerp(followTransform.transform.rotation, nextRotation, Time.deltaTime * rotationLerp);
+
+            if (move.x == 0 && move.y == 0)
+            {
+                nextPosition = transform.position;
+                return; 
+            }
+
+            var moveSpeed = speed / 100f;
+            var position = (transform.forward * move.y * moveSpeed) + (transform.right * move.x * moveSpeed);
+            nextPosition = transform.position + position;        
+        
+
+            //Set the player rotation based on the look transform
+            transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+            //reset the y rotation of the look transform
+            followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+        }
+
+        private void FixedUpdate()
+        {
+            var worldDeltaPosition = nextPosition - transform.position;
+            
+            //Map to local space
+            var dX = Vector3.Dot(transform.right, worldDeltaPosition);
+            var dY = Vector3.Dot(transform.forward, worldDeltaPosition);
+            var deltaPosition = new Vector2(dX, dY);
+            
+            float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+            smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+
+            if (Time.deltaTime > 1e-5f)
+            {
+                velocity = smoothDeltaPosition / Time.deltaTime;
+            }
+
+            transform.position = nextPosition;
+        }
+    }
 }
